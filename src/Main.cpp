42d8fbada119d71b4c34918eb399c43bfd72afbd
Neptune Magic Sauce <37757244+NeptuneMagicSauce@users.noreply.github.com>
2025-07-01 22:18:53 +0200

    rewrite Main in functional rather than Object style

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,145 +18,132 @@
 
 using namespace std;
 
-struct Main {
-  Main(int& argc, char** argv);
-  void connectSignals();
-  // TODO no need to both declare and define
-  QApplication* app = nullptr;
-  LoadCell* loadcell = nullptr;
-  Weight* weight = nullptr;
-  Calibration* calibration = nullptr;
-  Logic* logic = nullptr;
-  Delay* delay = nullptr;
-  ToolBar* toolbar = nullptr;
-  Debug* debug = nullptr;  // after all settings are loaded
-  MainScreen* mainscreen = nullptr;
-  CentralWidget* central = nullptr;
-  MainWindow* window = nullptr;
-};
-
 int main(int argc, char** argv) {
-  auto main = Main{argc, argv};
+  QApplication* app = new QApplication(argc, argv);
+  LoadCell* loadcell = new LoadCell;
+  Weight* weight = new Weight;
+  Calibration* calibration = new Calibration;
+  Logic* logic = new Logic;
+  Delay* delay = new Delay;
+  ToolBar* toolbar = new ToolBar;
+  Debug* debug = new Debug;
+  MainScreen* mainscreen = new MainScreen{weight, delay};
+  CentralWidget* central =
+      new CentralWidget{{new SubScreen("Main", mainscreen), new SubScreen("Calibration", calibration),
+                         new SubScreen("Debug", debug)}};
+  MainWindow* window = new MainWindow{central, toolbar};
 
-  auto ret = main.app->exec();
+  // Initialize globals
+  {
+    app->setStyleSheet("QWidget{font-size: 48pt;} ");
 
-  main.logic->reset();
+    // set up initial values
+    delay->setDelay(logic->delaySeconds());
+    logic->hasGPIO = loadcell->hasGPIO();
+
+    window->show();  // must be after window is  finished constructing and after setStyleSheet
+  }
+
+  // Connect Signals
+  {
+    // Shortcuts
+    auto quitShortcut = new QShortcut(QKeySequence(Qt::Key_F12), window);
+    quitShortcut->setContext(Qt::ApplicationShortcut);
+    QObject::connect(quitShortcut, &QShortcut::activated, app, &QApplication::quit);
+    auto fullscreenShortcut = new QShortcut(QKeySequence(Qt::Key_F11), window);
+    fullscreenShortcut->setContext(Qt::ApplicationShortcut);
+    QObject::connect(fullscreenShortcut, &QShortcut::activated, toolbar->fullscreen, &QAction::trigger);
+
+    // ToolBar
+    QObject::connect(toolbar->quit, &QAction::triggered, app, &QApplication::quit);
+    QObject::connect(toolbar->fullscreen, &QAction::triggered, [&] {
+      auto isFullscreen = false;
+      window->toggleFullscreen(isFullscreen);
+      toolbar->fullscreen->setIcon(ToolBar::fullScreenIcon(isFullscreen));
+    });
+    QObject::connect(toolbar->calibration, &QAction::triggered, [&]() { central->setPage(calibration); });
+    QObject::connect(toolbar->debug, &QAction::triggered, [&] { central->setPage(debug); });
+
+    // Dispense
+    QObject::connect(mainscreen->dispenseButton, &QAbstractButton::released, [&]() {
+      mainscreen->dispenseButton->setEnabled(false);
+      logic->manualDispense();
+    });
+
+    // LoadCell
+    QObject::connect(loadcell->timer, &QTimer::timeout, [&]() {
+      // TODO decouple logic loop from sampling loop
+      // TODO tick right away, dont wait a first interval
+      auto dispensed = false;
+      auto tare = weight->tare();
+      auto weightTarred = 0.0;
+      if (auto data = loadcell->read()) {
+        weight->update(data.value().value, weightTarred);
+        calibration->update(data.value().reading);
+        logic->update(weightTarred, tare, dispensed);
+        if (dispensed) {
+          mainscreen->dispenseButton->setEnabled(false);
+        }
+      } else {
+        weight->update({}, weightTarred);
+        calibration->update({});
+        logic->update({}, tare, dispensed);
+      }
+
+      delay->setRemaining(logic->timeToDispense());
+    });
+
+    // Weight
+    weight->connect();
+    QObject::connect(weight->eventTareFinished(), &QTimer::timeout,
+                     [&] { central->statusMessage(weight->messageFinished); });
+
+    // SubScreens
+    SubScreen::connect([&] { central->setPage(mainscreen); });
+
+    // Calibration
+    QObject::connect(calibration->buttons.step1, &QAbstractButton::released,
+                     [&] { calibration->callbacks.step1(loadcell->readPreciseRaw()); });
+    QObject::connect(calibration->buttons.step2, &QAbstractButton::released, [&] {
+      QString status;
+      if (auto readingKnown = loadcell->readPreciseRaw()) {
+        if (auto calibrationData = calibration->callbacks.step2(readingKnown.value())) {
+          loadcell->recalibrate(calibrationData.value(), status);
+        }
+      } else {
+        status = "Failed to read the weight";
+      }
+      central->setPage(mainscreen);
+      central->statusMessage(status);
+    });
+    calibration->connect();
+
+    // Logic
+    logic->connect();
+    QObject::connect(logic->timerEndDispense(), &QTimer::timeout,
+                     [&] { mainscreen->dispenseButton->setEnabled(true); });
+
+    // Delay
+    delay->connect();
+    QObject::connect(delay->delayDial, &DeltaDial::valueChanged, [&] {
+      logic->changeDelay(delay->delayDial->delta);
+      delay->setDelay(logic->delaySeconds());
+    });
+  }
+
+  // Initializing that needs the signals connected
+  {
+    if (window->isSmallScreen) {
+      toolbar->fullscreen->trigger();
+    } else {
+      window->setAutomaticSize();
+    }
+  }
+
+  auto ret = app->exec();
+
+  // Clean Up
+  logic->reset();
 
   return ret;
-}
-
-Main::Main(int& argc, char** argv)
-    : app(new QApplication(argc, argv)),
-      loadcell(new LoadCell),
-      weight(new Weight),
-      calibration(new Calibration),
-      logic(new Logic),
-      delay(new Delay),
-      toolbar(new ToolBar),
-      debug(new Debug),
-      mainscreen(new MainScreen(weight, delay)),
-      // do not have a title for the main screen
-      central(new CentralWidget({new SubScreen("Main", mainscreen), new SubScreen("Calibration", calibration),
-                                 new SubScreen("Debug", debug)})),
-      window(new MainWindow(central, toolbar)) {
-  app->setStyleSheet("QWidget{font-size: 48pt;} ");
-
-  // set up initial values
-  delay->setDelay(logic->delaySeconds());
-  logic->hasGPIO = loadcell->hasGPIO();
-
-  window->show();    // must be after window is  finished constructing and after setStyleSheet
-  connectSignals();  // must be after all members are constructed
-
-  if (window->isSmallScreen) {
-    toolbar->fullscreen->trigger();
-  } else {
-    window->setAutomaticSize();
-  }
-}
-
-void Main::connectSignals() {
-  // Shortcuts
-  auto quitShortcut = new QShortcut(QKeySequence(Qt::Key_F12), window);
-  quitShortcut->setContext(Qt::ApplicationShortcut);
-  QObject::connect(quitShortcut, &QShortcut::activated, app, &QApplication::quit);
-  auto fullscreenShortcut = new QShortcut(QKeySequence(Qt::Key_F11), window);
-  fullscreenShortcut->setContext(Qt::ApplicationShortcut);
-  QObject::connect(fullscreenShortcut, &QShortcut::activated, toolbar->fullscreen, &QAction::trigger);
-
-  // ToolBar
-  QObject::connect(toolbar->quit, &QAction::triggered, app, &QApplication::quit);
-  QObject::connect(toolbar->fullscreen, &QAction::triggered, [&] {
-    auto isFullscreen = false;
-    window->toggleFullscreen(isFullscreen);
-    toolbar->fullscreen->setIcon(ToolBar::fullScreenIcon(isFullscreen));
-  });
-  QObject::connect(toolbar->calibration, &QAction::triggered, [&]() { central->setPage(calibration); });
-  QObject::connect(toolbar->debug, &QAction::triggered, [&] { central->setPage(debug); });
-
-  // Dispense
-  QObject::connect(mainscreen->dispenseButton, &QAbstractButton::released, [this]() {
-    mainscreen->dispenseButton->setEnabled(false);
-    logic->manualDispense();
-  });
-
-  // LoadCell
-  QObject::connect(loadcell->timer, &QTimer::timeout, [&]() {
-    // TODO decouple logic loop from sampling loop
-    // TODO tick right away, dont wait a first interval
-    auto dispensed = false;
-    auto tare = weight->tare();
-    auto weightTarred = 0.0;
-    if (auto data = loadcell->read()) {
-      weight->update(data.value().value, weightTarred);
-      calibration->update(data.value().reading);
-      logic->update(weightTarred, tare, dispensed);
-      if (dispensed) {
-        mainscreen->dispenseButton->setEnabled(false);
-      }
-    } else {
-      weight->update({}, weightTarred);
-      calibration->update({});
-      logic->update({}, tare, dispensed);
-    }
-
-    delay->setRemaining(logic->timeToDispense());
-  });
-
-  // Weight
-  weight->connect();
-  QObject::connect(weight->eventTareFinished(), &QTimer::timeout,
-                   [&] { central->statusMessage(weight->messageFinished); });
-
-  // SubScreens
-  SubScreen::connect([&] { central->setPage(mainscreen); });
-
-  // Calibration
-  QObject::connect(calibration->buttons.step1, &QAbstractButton::released,
-                   [&] { calibration->callbacks.step1(loadcell->readPreciseRaw()); });
-  QObject::connect(calibration->buttons.step2, &QAbstractButton::released, [&] {
-    QString status;
-    if (auto readingKnown = loadcell->readPreciseRaw()) {
-      if (auto calibrationData = calibration->callbacks.step2(readingKnown.value())) {
-        loadcell->recalibrate(calibrationData.value(), status);
-      }
-    } else {
-      status = "Failed to read the weight";
-    }
-    central->setPage(mainscreen);
-    central->statusMessage(status);
-  });
-  calibration->connect();
-
-  // Logic
-  logic->connect();
-  QObject::connect(logic->timerEndDispense(), &QTimer::timeout,
-                   [&] { mainscreen->dispenseButton->setEnabled(true); });
-
-  // Delay
-  delay->connect();
-  QObject::connect(delay->delayDial, &DeltaDial::valueChanged, [&] {
-    logic->changeDelay(delay->delayDial->delta);
-    delay->setDelay(logic->delaySeconds());
-  });
 }
