6ca0b0af3721aabbf1b2fca1220b049d9c9081b8
Neptune Magic Sauce <37757244+NeptuneMagicSauce@users.noreply.github.com>
2025-07-02 23:51:46 +0200

    Setting widget: create members

--- src/Debug.cpp
+++ src/Debug.cpp
@@ -3,10 +3,13 @@
 #include <QGridLayout>
 #include <QLabel>
 #include <QPushButton>
+#include <QToolButton>
 #include <iostream>
 
+#include "DeltaDial.hpp"
 #include "Settings.hpp"
 #include "SubScreen.hpp"
+#include "Widget.hpp"
 
 namespace {
 
@@ -14,10 +17,10 @@ auto populated = false;
 auto buttons = QMap<QString, QPushButton*>{};
 auto screens = QMap<QString, SubScreen*>{};
 
-auto formatKey(QString key) {
+auto lineBreaked(const QString& line, auto maxLength) {
   auto asWords = QString{};
   auto prev = QChar{' '};
-  for (auto c : key) {
+  for (auto c : line) {
     if (c.isUpper() || (c.isDigit() && prev.isDigit() == false)) {
       asWords += " ";
     }
@@ -28,7 +31,7 @@ auto formatKey(QString key) {
   auto ret = QString{};
   auto lineLength = 0;
   for (auto word : asWords.split(" ")) {
-    auto lineBreak = lineLength + word.length() > 21;
+    auto lineBreak = lineLength + word.length() > maxLength;
     if (lineBreak) {
       ret += "\n";
       lineLength = 0;
@@ -45,24 +48,33 @@ bool Debug::Populated() { return populated; }
 
 struct Setting : public QWidget {
   const QString key;
+  QLabel* description = new QLabel;
   QLabel* value = new QLabel;
-  QPushButton* resetButton = new QPushButton{"Reset"};
+  QLabel* unit = new QLabel;
+  QLabel* isDefault = new QLabel;
+  QToolButton* resetButton = new QToolButton;
+  DeltaDial* changeButton = new DeltaDial;
 
   Setting(QString key) : key(key) {
+    Widget::FontSized(this, 15);
+    // TODO here:
+    //   pass init values to Settings::load() rather than get()
+    //   support for callback on change
+    // TODO DeltaDial: bigger maximum for smooth rotate, then divide delta
+    description->setText(lineBreaked(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore "
+        "et dolore magna aliqua.",
+        20));
+    value->setText("1.23456");
+    unit->setText("Milliseconds");
+    isDefault->setText("Yes");
+    resetButton->setText("Reset");
+
     auto layout = new QHBoxLayout;
     setLayout(layout);
-    value->setText("foo");  // debug
-    layout->addWidget(value);
-    layout->addWidget(resetButton);
-    // TODO here
-    //  alignment of layout is bad, too much blank space
-    // per item:
-    //  value
-    //  is default ?
-    //  unit (milliseconds, ...)
-    //  reset = set default (QToolButton)
-    //  support for callback on change
-    //  custom value with dial
+    for (auto widget : QList<QWidget*>{description, value, unit, isDefault, resetButton, changeButton}) {
+      layout->addWidget(widget);
+    }
   }
 };
 
@@ -77,7 +89,7 @@ Debug::Debug() {
     std::cout << "Setting: " << key.toStdString() << " = " << Settings::get(key, 0).toString().toStdString()
               << std::endl;
 
-    auto name = formatKey(key);
+    auto name = lineBreaked(key, 12);
     auto row = index / itemsPerRow;
     auto column = index % itemsPerRow;
     auto button = new QPushButton{name};
@@ -85,7 +97,7 @@ Debug::Debug() {
     layout->addWidget(button, row, column);
     buttons[key] = button;
 
-    auto screenContents = new QWidget;  // todo
+    auto screenContents = new Setting{key};
     auto screen = new SubScreen{name.replace("\n", ""), screenContents};
     screens[key] = screen;
 
